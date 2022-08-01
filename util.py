f4bc639c1a82366b7d786170234b8e9559061027
mohamedfadlalla <68976942+mohamedfadlalla@users.noreply.github.com>
2022-08-01 14:20:24 +0100

    Update util.py
    added function to download papers using doi from sci-hub

--- util.py
+++ util.py
@@ -5,3 +5,13 @@ def to_str(l):
     for i in l:
         s =s+i+' '
     return s
+
+def download_paper(doi, name):
+    #download papers form scihub using doi
+    name = name + '.pdf'
+    url = f'https://sci-hub.se/{doi}'
+    response = requests.get(url)
+    soup = bs(response.content)
+    url = 'http:' + soup.find('div', id= 'buttons').button['onclick'].split("'")[1]
+    response = requests.get(url)
+    open(name, "wb").write(response.content)

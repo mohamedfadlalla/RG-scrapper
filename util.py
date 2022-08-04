c336cc3a7987f607caf777e8de461ce21177a1bc
mohamedfadlalla <68976942+mohamedfadlalla@users.noreply.github.com>
2022-08-04 20:26:20 +0100

    Update util.py
    added get_email

--- util.py
+++ util.py
@@ -1,3 +1,8 @@
+import requests
+import re
+
+from PyPDF2 import PdfFileReader
+from bs4 import BeautifulSoup as bs
 
 
 def to_str(l):
@@ -8,20 +13,26 @@ def to_str(l):
 
 def download_paper(doi, name):
     #download papers form scihub using doi
-    name = name + '.pdf'
     url = f'https://sci-hub.se/{doi}'
+    print(url)
     response = requests.get(url)
     soup = bs(response.content)
-    url = 'http:' + soup.find('div', id= 'buttons').button['onclick'].split("'")[1]
-    response = requests.get(url)
-    open(name, "wb").write(response.content)
+    url =  soup.find('div', id= 'buttons').button['onclick'].split("'")[1]
+    if url.startswith('https://sci-hub.se/'):
+      response = requests.get(url)
+      open(name, "wb").write(response.content)
+    else:
+      url = 'https://sci-hub.se/' + url
+      response = requests.get(url)
+      open(name, "wb").write(response.content)
+
 
     
     
 def get_email(file):
     #extract emails from pdfs
     pattren = '\w+@\w+\.\w+'
-    reader = PdfReader("test2.pdf")
+    reader = PdfFileReader(file)
     text = ""
     for page in reader.pages:
         text += page.extract_text() + "\n"

845833a96314024d6f6da1e5724065844436e78f
mohamedfadlalla <68976942+mohamedfadlalla@users.noreply.github.com>
2022-08-07 08:10:02 +0100

    updated downlaod_paper on util.py

--- util.py
+++ util.py
@@ -17,12 +17,15 @@ def download_paper(doi, name):
     print(url)
     response = requests.get(url)
     soup = bs(response.content)
-    url =  soup.find('div', id= 'buttons').button['onclick'].split("'")[1]
-    if url.startswith('https://sci-hub.se/'):
+    url =  soup.find('div', id= 'buttons').button['onclick'].split("'")[1].replace('//','')
+    print('before editing', url)
+    if url.startswith('zero.sci-hub.se'):
+      url = 'http://' + url
       response = requests.get(url)
       open(name, "wb").write(response.content)
     else:
       url = 'https://sci-hub.se/' + url
+      print('after editing: ',url)
       response = requests.get(url)
       open(name, "wb").write(response.content)
 
